c7a7e408270f4c902b0f3958dc693acc8e97dc28
Mario <20210050@ricaldone.edu.sv>
2026-06-26 17:11:09 -0600

    feat: routes terminado

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,21 +1,20 @@
 import express from "express"
-import ordersController from "../controllers/orders.js";
+import usersController from "../controllers/users.js";
 import { validateAuthCookie } from "../middlewares/authMiddleware.js";
 
 //Router() nos ayuda a colocar los métodos que tendrá el endpoint
 
 const router = express.Router();
 
-//(api/orders/)
+//(api/users/)
 router.route("/")
-.get(ordersController.getOrders)
-.post(ordersController.createOrder)
+.get(usersController.getUsers)
 
 //Definimos los method para el endpoint que includes un parámetro dinámico ":id". Este parámetro se utiliza para identificar un recurso específico, como un producto en este caso. Los métodos PUT y DELETE se utilizan para actualizar y eliminar un recurso específico identificado por su ID, respectivamente. 
-//(api/orders/:id)
+//(api/users/:id)
 router.route("/:id")
-.put( ordersController.updateOrder)
-.delete( ordersController.deleteOrder)
-.get( ordersController.getOrderById)
+.put( usersController.updateUser)
+.delete( usersController.deleteUser)
+.get( usersController.getUserById)
 
 export default router;
