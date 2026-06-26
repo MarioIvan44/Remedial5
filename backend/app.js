27284f515438e9dd842f2eff387a0010c0a12005
mario <20210050@ricaldone.edu.sv>
2026-06-26 15:29:05 -0600

    feat: initial commit

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+//Importar express
+import express from 'express';
+
+import cors from 'cors'; 
+//Importante 
+import cookieParser from 'cookie-parser';
+
+//Ejecutar express
+const app = express();
+
+//Use cors is for allow cross-origin requests, which is necessary when the frontend and backend are hosted on different domains or ports. It enables the frontend to make API calls to the backend without being blocked by the browser's same-origin policy.
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://localhost:5174'], // This is the URL of the frontend application that is allowed to access the backend API.
+    //Allows the sending of cookies and other credentials in cross-origin requests, which is necessary for authentication and session management.
+    credentials: true
+}));
+ 
+//Cookie parser allows us to parse the cookies sent by the client in the request headers and make them easily accessible in our route handlers. This is particularly useful for handling authentication tokens, session IDs, and other data stored in cookies. By using cookie-parser, we can read and manipulate cookies in our Express application, enabling features like user authentication and personalized experiences based on cookie data.
+app.use(cookieParser());
+
+//Acepta JSON 
+app.use(express.json());
+
+app.use(limiter);
+
+//Creamos los endpoints
+
+
+export default app;
